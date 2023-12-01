8c2bb0585e4723b71ec0d15da50c676aabc8f553
lynn <lynn@163.com>
2023-12-01 17:56:34 +0800

    验证了一个视图可以绑定多个路由

--- demo/hello/hello.py
+++ demo/hello/hello.py
@@ -3,6 +3,17 @@ from flask import *
 hello = Flask(__name__)
 
 
-@hello.route()
+@hello.route('/')
 def index():
     return "<h1>hello world</h1>"
+
+
+@hello.route('/nihao')
+@hello.route('/hellworld')
+# 同一个视图被绑定多个路由
+def helloworld():
+    return "<h1>你好啊</h1>"
+
+
+if __name__ == '__main__':
+    hello.run(debug=True)
