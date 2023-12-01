66f62955ae5c4d27d72749c547f52e852fde6c96
lynn <lynn@163.com>
2023-12-01 18:01:08 +0800

    验证路由规则中可以存在变量

--- demo/hello/hello.py
+++ demo/hello/hello.py
@@ -15,5 +15,9 @@ def helloworld():
     return "<h1>你好啊</h1>"
 
 
+@hello.route('/greet/<name>')
+#URL中可以包含变量，将传入app.route()的字符串称为URL规则，而不是URL
+def greet(name):
+    return '<h1> hello  {} </h1>'.format(name)
 if __name__ == '__main__':
     hello.run(debug=True)
